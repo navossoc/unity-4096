622abcb8d5810160fc8f2a603fa26b1348db451b
Rafael Cossovan <navossoc@gmail.com>
2015-05-29 10:38:40 -0300

    Added Main screen draft.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("[Menu] Play");
+        Application.LoadLevel("Game");
+    }
+
+    public void Options()
+    {
+        Debug.Log("[Menu] Options");
+        Application.LoadLevel("Options");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("[Menu] Quit");
+        Application.Quit();
+    }
+}
