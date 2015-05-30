f66f165f1028bf512b5540b9a01c59085a1f7c3b
Rafael Cossovan <navossoc@gmail.com>
2015-05-29 21:07:21 -0300

    Added Game screen draft. Added TileManager and Tile components.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    private int value = 0;
+
+    private Image tileImage;
+    private Text tileText;
+
+    private static Color32[] colorTable =
+    {
+        new Color32(204, 192, 179, 255),    // 1 (none)
+        new Color32(238, 228, 218, 255),    // 2
+        new Color32(237, 224, 200, 255),    // 4
+        new Color32(242, 177, 121, 255),    // 8
+        new Color32(245, 149, 99, 255),     // 16
+        new Color32(246, 124, 95, 255),     // 32
+        new Color32(246, 94, 59, 255),      // 64
+        new Color32(237, 207, 114, 255),    // 128
+        new Color32(237, 204, 97, 255),     // 256
+        new Color32(237, 200, 80, 255),     // 512
+        new Color32(237, 197, 63, 255),     // 1024
+        new Color32(237, 194, 46, 255),     // 2048
+        new Color32(237, 194, 46, 255),     // 4096
+    };
+
+    // Use this for initialization
+    void Awake()
+    {
+        tileImage = GetComponent<Image>();
+        tileText = GetComponentInChildren<Text>();
+    }
+
+    /// <summary>
+    /// Get tile value
+    /// </summary>
+    /// <returns>return an int</returns>
+    public int GetValue()
+    {
+        return value > 0 ? (1 << value) : 0;
+    }
+
+    /// <summary>
+    /// Set Tile value
+    /// </summary>
+    /// <param name="number">tile value (power of 2)</param>
+    public void SetValue(int number)
+    {
+        if (number < 0 || number > 12)
+        {
+            Debug.LogErrorFormat("[{0}] Number {1} out of range", name, number);
+        }
+        number = Mathf.Clamp(number, 0, 12);
+
+        value = number;
+        if (value > 0)
+        {
+            tileText.text = (1 << value).ToString();
+        }
+        else
+        {
+            tileText.text = "";
+        }
+
+        UpdateColor();
+    }
+
+    /// <summary>
+    /// Change Tile color based on his value
+    /// </summary>
+    private void UpdateColor()
+    {
+        // Background
+        tileImage.color = colorTable[value];
+        // Text color
+        if (value <= 2)
+        {
+            tileText.color = new Color32(119, 110, 101, 255);
+        }
+        else
+        {
+            tileText.color = new Color32(249, 246, 242, 255);
+        }
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TileManager : MonoBehaviour
+{
+    // Tiles panel (parent)
+    public Transform TilesParent;
+    public GameObject TilePrefab;
+
+    // Configuration
+    private const int tilesParentPadding = 16;
+    private const int tilesPerRow = 4;
+    private const int tileWidth = 96, tileHeight = 96;
+    private const int tileMargin = 8;
+
+    // Number of tiles
+    private const int totalTiles = tilesPerRow * tilesPerRow;
+
+    // Tile array reference
+    private Tile[] tileObjects;
+    // Tile array values
+    private int[] tileValues;
+
+    // Use this for initialization
+    void Start()
+    {
+        // HACK: Clear all
+        foreach (Transform child in transform)
+        {
+            DestroyObject(child.gameObject);
+        }
+
+        // Allocate memory
+        tileObjects = new Tile[totalTiles];
+        tileValues = new int[totalTiles];
+
+        int x = tilesParentPadding, y = -tilesParentPadding;
+
+        // For each cell
+        for (int i = 0; i < totalTiles; i++)
+        {
+            // Create a new tile
+            GameObject tile = Instantiate<GameObject>(TilePrefab);
+            tile.name = "Tile " + i;
+
+            RectTransform tileRect = tile.GetComponent<RectTransform>();
+
+            // Tile in a row
+            if (i % tilesPerRow == 0)
+            {
+                x = tilesParentPadding;
+            }
+            else
+            {
+                x += tileWidth + tileMargin;
+            }
+
+            // Tile in a column
+            if (i % tilesPerRow == 0 && i > 0)
+            {
+                y -= tileHeight + tileMargin;
+            }
+
+            // Set parent
+            tileRect.SetParent(TilesParent, false);
+
+            // Move tile
+            tileRect.Translate(x, y, 0);
+
+            // Assign tile
+            tileObjects[i] = tile.GetComponent<Tile>();
+            tileObjects[i].SetValue(0);
+            tileValues[i] = 0;
+        }
+
+        // Panel
+        x += tileWidth + tilesParentPadding;
+        y -= tileHeight + tilesParentPadding;
+        y = Mathf.Abs(y);
+
+        // Resize
+        RectTransform panelRect = GetComponent<RectTransform>();
+        panelRect.sizeDelta = new Vector2(x, y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int horizontal = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
+        int vertical = Mathf.RoundToInt(Input.GetAxis("Vertical"));
+    }
+}
