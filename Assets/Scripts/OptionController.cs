90979fffc17b55692249e0d182eca0230e7cc223
Rafael Cossovan <navossoc@gmail.com>
2015-06-10 20:47:10 -0300

    Added UI controls for Options Screen.

--- Assets/Scripts/OptionController.cs
+++ Assets/Scripts/OptionController.cs
@@ -1,7 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionController : MonoBehaviour
 {
+    /*
+     * Fields
+     */
+
+    private Slider animationSlider, soundSlider;
+    private Text animationText, soundText;
+
+    /*
+     * Enums
+     */
+
+    public enum AnimationEffect
+    {
+        None,
+        Simple,
+        Complete
+    }
+
+    public enum SoundEffect
+    {
+        Off,
+        On
+    }
+
     /*
      * Methods
      */
@@ -9,5 +34,30 @@ public class OptionController : MonoBehaviour
     public void ReturnToMain()
     {
         Application.LoadLevel("Main");
+    }
+
+    public void UpdateAnimationSlider()
+    {
+        AnimationEffect option = (AnimationEffect)animationSlider.value;
+        animationText.text = option.ToString();
+    }
+
+    public void UpdateSoundSlider()
+    {
+        SoundEffect option = (SoundEffect)soundSlider.value;
+        soundText.text = option.ToString();
+    }
+
+    private void Start()
+    {
+        // Animation effects
+        animationSlider = GameObject.Find("Animation/Slider").GetComponent<Slider>();
+        animationText = GameObject.Find("Animation/Value").GetComponent<Text>();
+        UpdateAnimationSlider();
+
+        // Sound effects
+        soundSlider = GameObject.Find("Sound/Slider").GetComponent<Slider>();
+        soundText = GameObject.Find("Sound/Value").GetComponent<Text>();
+        UpdateSoundSlider();
     }
 }
