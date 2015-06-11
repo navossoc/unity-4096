0ff935f7b40aed339a49ea1e3927463df09d6164
Rafael Cossovan <navossoc@gmail.com>
2015-06-10 23:41:11 -0300

    Added code to handle player preferences.

--- Assets/Scripts/OptionController.cs
+++ Assets/Scripts/OptionController.cs
@@ -7,6 +7,12 @@ public class OptionController : MonoBehaviour
      * Fields
      */
 
+    private const int AnimationEffectDefault = 2;
+    private const int SoundEffectDefault = 0;
+
+    private const string AnimationEffectKey = "AnimationEffect";
+    private const string SoundEffectKey = "SoundEffect";
+
     private Slider animationSlider, soundSlider;
     private Text animationText, soundText;
 
@@ -31,17 +37,34 @@ public class OptionController : MonoBehaviour
      * Methods
      */
 
-    public void ReturnToMain()
+    /// <summary>
+    /// Apply button (save changes)
+    /// </summary>
+    public void ApplyButton()
     {
-        Application.LoadLevel("Main");
+        SaveSettings();
     }
 
+    /// <summary>
+    /// Cancel button (don't save any changes)
+    /// </summary>
+    public void CancelButton()
+    {
+        ReturnToMain();
+    }
+
+    /// <summary>
+    /// Update text value on Animation Slider
+    /// </summary>
     public void UpdateAnimationSlider()
     {
         AnimationEffect option = (AnimationEffect)animationSlider.value;
         animationText.text = option.ToString();
     }
 
+    /// <summary>
+    /// Update text value on Sound Slider
+    /// </summary>
     public void UpdateSoundSlider()
     {
         SoundEffect option = (SoundEffect)soundSlider.value;
@@ -53,11 +76,55 @@ public class OptionController : MonoBehaviour
         // Animation effects
         animationSlider = GameObject.Find("Animation/Slider").GetComponent<Slider>();
         animationText = GameObject.Find("Animation/Value").GetComponent<Text>();
-        UpdateAnimationSlider();
 
         // Sound effects
         soundSlider = GameObject.Find("Sound/Slider").GetComponent<Slider>();
         soundText = GameObject.Find("Sound/Value").GetComponent<Text>();
+
+        // Read settings from player preferences
+        ReadSettings();
+    }
+
+    /// <summary>
+    /// Read settings from PlayerPrefs and update UI
+    /// </summary>
+    private void ReadSettings()
+    {
+        // Read values from player preferences
+        int animation = PlayerPrefs.GetInt(AnimationEffectKey, AnimationEffectDefault);
+        int sound = PlayerPrefs.GetInt(SoundEffectKey, SoundEffectDefault);
+
+        // Update slider value
+        animationSlider.value = animation;
+        UpdateAnimationSlider();
+
+        // Update slider value
+        soundSlider.value = sound;
         UpdateSoundSlider();
+    }
+
+    /// <summary>
+    /// Save settings to PlayerPrefs
+    /// </summary>
+    private void SaveSettings()
+    {
+        // Read values from sliders
+        int animation = (int)animationSlider.value;
+        int sound = (int)soundSlider.value;
+
+        // Save settings on player preferences
+        PlayerPrefs.SetInt(AnimationEffectKey, animation);
+        PlayerPrefs.SetInt(SoundEffectKey, sound);
+
+        // Go back to main menu
+        ReturnToMain();
+    }
+
+    /// <summary>
+    /// Go to Main Scene
+    /// </summary>
+    private void ReturnToMain()
+    {
+        Application.LoadLevel("Main");
     }
 }
